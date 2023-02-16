7134e3355c3c94154230425b8dd61f7bfb436cf2
Prayash Koirala <prayashkoirala07@gmail.com>
2023-02-16 14:25:13 +0545

    color theme implementation

--- lib/features/homepage/presentation/pages/homepage_view.dart
+++ lib/features/homepage/presentation/pages/homepage_view.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_theme/core/theme/custom_color_theme.dart';
 
 class HomePageView extends StatelessWidget {
   const HomePageView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final appColorTheme = Theme.of(context).extension<CustomColors>();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Homepage"),
-      ),
-      body: const Center(
-        child: Text("HomePage Data"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text(
+            "Homepage",
+          ),
+        ),
+        body: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              height: 200,
+              color: appColorTheme?.success,
+            ),
+            Positioned(
+              bottom: -40,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Center(
+                  child: Container(
+                    height: 100,
+                    width: MediaQuery.of(context).size.width * .8,
+                    decoration: BoxDecoration(
+                      color: appColorTheme?.warning,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
